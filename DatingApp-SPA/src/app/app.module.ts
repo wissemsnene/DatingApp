d5dc33bd59ec818b18e76d9ca35c08ebc76fa295
Wissem Snene <wissem.snene@gmail.com>
2018-11-11 14:38:19 +0100

    End of section 7

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
@@ -12,6 +13,11 @@ import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./services/error.interceptor";
 import { AlertifyService } from "./services/alertify.service";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { MemberListComponent } from "./member-list/member-list.component";
+import { ListsComponent } from "./lists/lists.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { appRoutes } from "./routes";
+import { AuthGuard } from "./_guards/auth.guard";
 
 @NgModule({
   declarations: [
@@ -19,15 +25,24 @@ import { BsDropdownModule } from "ngx-bootstrap/dropdown";
     ValueComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [AuthService, ErrorInterceptorProvider, AlertifyService],
+  providers: [
+    AuthService,
+    ErrorInterceptorProvider,
+    AlertifyService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
